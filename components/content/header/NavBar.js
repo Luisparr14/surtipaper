ce7198427fb6a511ffb1e8a7575a9ad383096b2b
Lucho <luisparr14@gmail.com>
2022-02-13 23:05:15 -0500

    fixed navbar

--- components/content/header/NavBar.js
+++ components/content/header/NavBar.js
@@ -2,8 +2,44 @@ import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import styles from '../../../styles/NavBar.module.css'
+
+const loadScript = () => {
+  console.log('LISTO')
+  let btn = document.getElementById('collapse-button');
+  let collapsenav = document.getElementById('collapseNav');
+  // let content = document.getElementById('content-page');
+  // let content = document.getElementsByClassName('content-page');
+  let linkColapseNav = document.getElementById('collapseNav').getElementsByTagName('a');
+
+  function toogleCollapse () {
+    collapsenav.classList.toggle('noCollapse')
+    btn.classList.toggle('collapseButtonActive');
+    // content.classList.toggle('blur')
+  }
+
+  btn.addEventListener('click', toogleCollapse)
+
+  // content.addEventListener('click', () => {
+  //   console.log('click')
+  //   if (collapsenav.classList.contains('noCollapse')) {
+  //     toogleCollapse()
+  //   }
+  // })
+
+  for (let i = 0; i < linkColapseNav.length; i++) {
+    linkColapseNav[i].addEventListener('click', () => {
+      if (collapsenav.classList.contains('noCollapse')) {
+        toogleCollapse()
+      }
+    })
+  }
+}
+
 export default function NavBar ({ href }) {
   const router = useRouter()
+  useEffect(() => {
+    loadScript()
+  }, [router])
   const styleSales = {
     color: router.asPath === "/sales" ? '#FF0505' : '#000'
   }
@@ -20,9 +56,6 @@ export default function NavBar ({ href }) {
 
   return (
     <>
-      <Head>
-        <script defer type='text/javascript' src='/js/main.js' ></script>
-      </Head>
       <nav className={styles.navbar}>
         <div className={styles.navbarNav}>
           <label className={styles.logo}>
